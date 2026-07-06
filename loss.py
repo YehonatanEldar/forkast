50e5d1f454b8c4eb855608a789ac6e4259f52ca5
YehonatanEldar <yehonatan.eldar2@gmail.com>
2026-07-06 12:42:42 +0300

    added backward func to loss

--- loss.py
+++ loss.py
@@ -2,9 +2,16 @@ from layer import Layer
 import numpy as np
 
 class Loss(Layer):
-    def forward(predicted: np.array, real: np.array) -> float:
+    def forward(self, predicted: np.array, real: np.array) -> float:
         """
         Calculates the MSE for the given batch
         """
 
-        return np.mean((predicted - real)**2)    
+        return np.mean((predicted - real)**2)
+    
+    def backward(self, predicted: np.array, real: np.array):
+        """
+        Calculates the derivative of MSE for the given batch
+        """
+
+        return (2/predicted.size) * (predicted - real)
