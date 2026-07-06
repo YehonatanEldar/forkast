7bb13defd26f84e53a0ca64ebfdd18bddb37d2b6
YehonatanEldar <yehonatan.eldar2@gmail.com>
2026-07-06 15:23:05 +0300

    added train function

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,12 @@
 from layer import Layer
 from dense import Dense
+from loss import Loss
 from activation import Activation, ActivationFuncs
 import numpy as np
-import json as js
+import json
+
 class NeuralNetwork:
-    def __init__(self, loss: Layer):
+    def __init__(self, loss: Loss):
         """
         intialize a neural network object
         """
@@ -27,16 +29,32 @@ class NeuralNetwork:
         """
         for curr_layer in self.layers:
             input_data = curr_layer.forward(input_data)
-        return self.layers[-1].input
+        return ActivationFuncs.sigmoid(self.layers[-1].input) # Apply sigmoid to final result
     
+    def train(self, x_train: np.array, y_train: np.array, epochs: int, learning_rate: float): # choo choo
+        """
+        Trains the network on the given batch
+        """
 
+        for _ in range(epochs):
+            # predict
+            results = self.predict(x_train)
 
-    def save(self, filepath: str): #TODO make save work
+            # calc loss
+            print("Error: " + self.loss.forward(results, y_train))
+
+            # propagate backwards
+            current_gradient = self.loss.backward()
+
+            for layer in self.layers[::-1]:
+                current_gradient = layer.backward(current_gradient, learning_rate)
+
+    def save(self, filepath: str): # TODO make save work
         """
         save a neural network to a file
         """
         with open(filepath + '.json', 'w') as save_file:
-            js.dump(save_file, self) 
+            json.dump(save_file, self) 
 
 
 
