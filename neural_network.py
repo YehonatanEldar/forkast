3d5b6eec93ebaa734ed2a70b719a89b6420eaa2f
someguy272 <noamon.levy@gmail.com>
2026-07-06 12:18:44 +0300

    added basic NN structure with predict and add

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,7 @@
 from layer import Layer
+from dense import Dense
+from activation import Activation, ActivationFuncs
+import numpy as np
 
 class NeuralNetwork:
     def __init__(self, loss: Layer):
@@ -8,12 +11,20 @@ class NeuralNetwork:
         self.layers = []
         self.loss = loss
 
-    def add(new_layer: Layer):
+    def add(self, new_layer: Dense):
         """
         append a new layer to the neural network
         """
+        self.layers.append(new_layer)
+        self.layers.append(Activation(ActivationFuncs.relu))
 
-    def predict(input_data):
-        pass
+    def predict(self, input_data: np.array) -> float:
+        """
+        Applies the network on a given input
+        """
+        for curr_layer in self.layers:
+            input_data = curr_layer.forward(input_data)
+        return input_data
+
 
 
