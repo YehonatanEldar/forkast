e3140501da5b84b3dc86f48f8a680a835100e75f
YehonatanEldar <yehonatan.eldar2@gmail.com>
2026-07-06 11:30:20 +0300

    added forward function to dense

---
+++ dense.py
@@ -0,0 +1,27 @@
+from layer import Layer
+import numpy as np
+
+class Dense(Layer):
+    def __init__(self, size: int, next_size: int):
+        """
+        Creates a new dense layer.
+        """
+        self.size = size
+        self.next_size = next_size
+        self.weights = np.zeros((next_size, size))
+        self.biases = np.zeros(size)
+        self.input = np.zeros(size)
+
+    def forward(self, input_arr: np.array) -> np.array:
+        """
+        Computes the output of the layer
+        """
+        output_arr = np.zeros(self.weights.next_size)
+        self.input = input_arr
+        self.input += self.biases
+
+        for output, weights in zip(output_arr, self.weights):
+            for input, weight in zip(self.input, weights):
+                output += input * weight
+
+        return output_arr
