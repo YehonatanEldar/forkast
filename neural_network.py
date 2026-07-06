779b33e160a7d8137e6afa4581d38225ca0d71ac
someguy272 <noamon.levy@gmail.com>
2026-07-06 12:01:31 +0300

    initalized neural network class

---
+++ neural_network.py
@@ -0,0 +1,19 @@
+from layer import Layer
+
+class NeuralNetwork:
+    def __init__(self, loss: Layer):
+        """
+        intialize a neural network object
+        """
+        self.layers = []
+        self.loss = loss
+
+    def add(new_layer: Layer):
+        """
+        append a new layer to the neural network
+        """
+
+    def predict(input_data):
+        pass
+
+
