40b19a40ed6a3348fbe34c9e9de7b297b363d5bc
someguy272 <noamon.levy@gmail.com>
2026-07-09 13:30:34 +0300

    fixed extra dict declaration in

--- activation.py
+++ activation.py
@@ -10,6 +10,7 @@ class ActivationFuncs(Enum):
     sigmoid = 'sigmoid'
 
 class Activation(Layer):
+    
     def __init__(self, activation_func: ActivationFuncs):
         """
         creates a new activation object
@@ -27,13 +28,14 @@ class Activation(Layer):
         """
         pass all activations in the input vector through the activation function
         """
+
         self.input = input
         output = self.activation_func(self.input)
         return output
     
-    def backward(self, output_gradient: np.array, learning_rate: float) -> np.array:
+    def backward(self, input: np.array, output_gradient: float, learning_rate: float) -> np.array:
         """
         makes the backward pass and changes the values, returns the new gradient
         """
-        next_gradient = -output_gradient * self.derivative_func(self.input)
+        next_gradient = -output_gradient * self.derivative_func(input.squeeze())
         return next_gradient
