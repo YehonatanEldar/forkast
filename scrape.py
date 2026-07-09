84ac6552b07592c821985603d34b7a3086ffe3dc
YehonatanEldar <yehonatan.eldar2@gmail.com>
2026-07-09 18:05:17 +0300

    better bing works!!!

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import os
 
 import requests
 from ddgs import DDGS
-
+from better_bing_image_downloader import downloader
 # Constants
 TARGET_WORD = "fork"
 HARD_NEGATIVES = ["spoon", "knife", "backscratcher", "ladle", "tong", "whisk"]
@@ -21,33 +21,44 @@ def get_image_urls(word: str, count: int) -> list[dict]:
     """
     with DDGS() as ddgs:
         return list(ddgs.images(word, max_results=round(count * SEARCH_BUFFER_FACTOR)))
+    
+
+# def download_images(results: list[str], count: int, output_dir: str):
+#     """
+#     Downloads the given url list to the given folder path
+#     """
+#     download_count = 0
+
+#     for item in results:
+#         if download_count >= count:
+#             break
+
+#         url = item['image']
+
+#         try: 
+#             response = requests.get(url)
+#             if response.status_code == 200:
+#                 # Copy the contents to a file
+#                 download_count += 1
+#                 with open(f"{output_dir}/{download_count}.jpg", "wb") as f:
+#                     f.write(response.content)
+
+#                 print(f"Downloaded: {download_count}/{count}")
 
 
-def download_images(results: list[str], count: int, output_dir: str):
-    """
-    Downloads the given url list to the given folder path
-    """
-    download_count = 0
+#         except Exception as e:
+#             print(e)
 
-    for item in results:
-        if download_count >= count:
-            break
-
-        url = item['image']
-
-        try: 
-            response = requests.get(url)
-            if response.status_code == 200:
-                # Copy the contents to a file
-                download_count += 1
-                with open(f"{output_dir}/{download_count}.jpg", "wb") as f:
-                    f.write(response.content)
-
-                print(f"Downloaded: {download_count}/{count}")
-
-
-        except Exception as e:
-            print(e)
+def download_images(query: str, count: int, output_dir: str):
+    downloader(
+    query=query, 
+    limit=count,                  # <--- Set this to whatever number you need (e.g., 60, 100, 200)
+    output_dir=output_dir,  # Destination folder
+    adult_filter_off=True, 
+    force_replace=False, 
+    timeout=50,
+    verbose=False
+)
 
 
 def main():
@@ -55,14 +66,14 @@ def main():
     os.makedirs(OUTPUT_DIR + '/positive', exist_ok=True)
     os.makedirs(OUTPUT_DIR + '/negative', exist_ok=True)
     # --- Scraping ---
+
     # 1. Scrape the target word
-    results = get_image_urls(TARGET_WORD, POS_COUNT)
-    print(f"Found {len(results)} result for {TARGET_WORD}!!!")
-    download_images(results, POS_COUNT, OUTPUT_DIR + '/positive')
+    download_images(TARGET_WORD, POS_COUNT, OUTPUT_DIR + '/positive')
+    print("Done!")
 
     # 2. Scrape from the hard negative list
-    for word in HARD_NEGATIVES:
-        results = get_image_urls(word, HARD_COUNT, OUTPUT_DIR + '/negative')
+    # for word in HARD_NEGATIVES:
+    #     results = get_image_urls(word, HARD_COUNT, OUTPUT_DIR + '/negative')
 
 if __name__ == '__main__':
     main()
